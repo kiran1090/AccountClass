dab1bff304b24f90c868f316ee375a7196aadaf1
kiran_a <iamkiranashok@gmail.com>
2022-12-28 20:05:43 +0530

    2nd commit

--- src/Account.java
+++ src/Account.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Account {
+public class Account {
+public String BankId,Name;
+public double Balance;
+public Account(String BankId, String Name,double Balance)
+{
+    this.BankId=BankId;
+    this.Name=Name;
+    this.Balance=Balance;
+}
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "BankId='" + BankId + '\'' +
+                ", Name='" + Name + '\'' +
+                ", Balance=" + Balance +
+                '}';
+    }
 }
--- src/TestAccount.java
+++ src/TestAccount.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class TestAccount {
+public class TestAccount {
+    public static void main(String [] args)
+    {
+        Account a1= new Account("P1","arun",100);
+        Account a2= new Account("P1","ann",100);
+        System.out.println("Account Details:"+a1);
+        System.out.println("Account Details:"+a2);
+    }
 }
