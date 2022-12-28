40e02fc46e447c3bf67a5d6c482c2709501d4d22
kiran_a <iamkiranashok@gmail.com>
2022-12-28 20:53:06 +0530

    Updates

--- src/Account.java
+++ src/Account.java
@@ -1,12 +1,24 @@
 public class Account {
 public String BankId,Name;
 public double Balance;
+public int credit,debit;
+
 public Account(String BankId, String Name,double Balance)
 {
     this.BankId=BankId;
     this.Name=Name;
     this.Balance=Balance;
+
 }
+public double credit(int i)
+{
+    return Balance=this.Balance+100;
+}
+public double debit(int i)
+{
+    return Balance -= 50;
+}
+
 
     @Override
     public String toString() {
@@ -15,5 +27,13 @@ public Account(String BankId, String Name,double Balance)
                 ", Name='" + Name + '\'' +
                 ", Balance=" + Balance +
                 '}';
+    }
+
+    public double transferTo(Account a2, int i) {
+
+    return Balance= a2.Balance+ 100;
+
+
+
     }
 }
