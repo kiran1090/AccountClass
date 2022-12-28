c8686c4582b22aa029bd3419b508182ef6ab7411
kiran1090 <63589265+kiran1090@users.noreply.github.com>
2022-12-28 20:06:39 +0530

    Update TestAccount.java

--- src/TestAccount.java
+++ src/TestAccount.java
@@ -5,5 +5,17 @@ public class TestAccount {
         Account a2= new Account("P1","ann",100);
         System.out.println("Account Details:"+a1);
         System.out.println("Account Details:"+a2);
+        // Test credit() and debit()
+      a1.credit(100);
+      System.out.println(a1);
+      a1.debit(50);
+      System.out.println(a1);
+      a1.debit(500);  // debit() error
+      System.out.println(a1);
+
+      // Test transfer()
+      a1.transferTo(a2, 100);  // toString()
+      System.out.println(a1);
+      System.out.println(a2);
     }
 }
